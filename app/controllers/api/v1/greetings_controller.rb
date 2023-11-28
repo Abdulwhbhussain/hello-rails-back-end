25477ca9a6888afe7c2d571be13bb07861045642
WHB901 <abdulwhbhussain@gmail.com>
2023-11-28 22:53:38 +0500

    Backend Complete

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -5,15 +5,3 @@ class Api::V1::GreetingsController < ApplicationController
     render json: { greeting: @greeting }
   end
 end
-
-# module Api
-#   module V1
-#     class GreetingsController < ApplicationController
-#       def random_greeting
-#         @greetings = Message.pluck(:greeting)
-#         @greeting = @greetings.sample
-#         render json: { greeting: @greeting }
-#       end
-#     end
-#   end
-# end
